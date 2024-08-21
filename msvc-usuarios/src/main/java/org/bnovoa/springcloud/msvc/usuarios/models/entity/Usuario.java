c9395f52e68e556047cb8abeff6efac7418f6652
b-novoa <brayannoga@gmail.com>
2024-08-21 21:22:00 +0200

    ADD Modelo Y Repositorio USUARIO

---
+++ msvc-usuarios/src/main/java/org/bnovoa/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -0,0 +1,23 @@
+package org.bnovoa.springcloud.msvc.usuarios.models.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+
+}
