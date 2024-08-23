048852b2b23d18a84746f694d702e75d6f9ac6cd
b-novoa <brayannoga@gmail.com>
2024-08-23 23:45:29 +0200

    ADD controlador, cambios en los properties

---
+++ msvc-usuarios/src/main/java/org/bnovoa/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,62 @@
+package org.bnovoa.springcloud.msvc.usuarios.controllers;
+
+import org.bnovoa.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.bnovoa.springcloud.msvc.usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listar(){
+        return usuarioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.findById(id);
+        if(usuario.isPresent()){
+            return ResponseEntity.ok().body(usuario.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity crear(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id) {
+        Optional<Usuario> o = usuarioService.findById(id);
+        if (o.isPresent()){
+            Usuario usuarioDB = o.get();
+            usuarioDB.setNombre(usuario.getNombre());
+            usuarioDB.setEmail(usuario.getEmail());
+            usuarioDB.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(usuarioDB));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Usuario> o = usuarioService.findById(id);
+        if (o.isPresent()) {
+            usuarioService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
--- msvc-usuarios/src/main/java/org/bnovoa/springcloud/msvc/usuarios/models/entity/Usuario.java
+++ msvc-usuarios/src/main/java/org/bnovoa/springcloud/msvc/usuarios/models/entity/Usuario.java
@@ -19,5 +19,35 @@ public class Usuario {
     @Column(name = "password")
     private String password;
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
